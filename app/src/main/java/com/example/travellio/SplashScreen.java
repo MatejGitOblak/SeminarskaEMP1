1f9bcfdd01242710fb893b1542f17ebeb0e8b8bc
Matej Oblak <74320191+MatejGitOblak@users.noreply.github.com>
2022-12-09 15:50:40 +0100

    Initial commit

---
+++ app/src/main/java/com/example/travellio/SplashScreen.java
@@ -0,0 +1,28 @@
+package com.example.travellio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+        
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this,MainActivity.class));
+                finish();
+            }
+        }, 2000);
+    }
+}
