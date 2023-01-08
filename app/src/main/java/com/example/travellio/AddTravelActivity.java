62a2c9780153b41cd3dda98f72fae659d537c361
Gasper Krevs <gasper.krevs@gmail.com>
2023-01-08 04:21:17 +0100

    n

--- app/src/main/java/com/example/travellio/AddTravelActivity.java
+++ app/src/main/java/com/example/travellio/AddTravelActivity.java
@@ -64,7 +64,7 @@ class Travel implements Serializable {
 
 public class AddTravelActivity extends AppCompatActivity {
 
-    ArrayList<String> travel_name, travel_info;
+    ArrayList<String> travel_id, travel_name, travel_info;
     DBHelper myDB;
 
     @Override
@@ -158,12 +158,32 @@ public class AddTravelActivity extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "Added Travel " + t.stays.get(0).country, Toast.LENGTH_LONG).show();
 
                     myDB = new DBHelper(AddTravelActivity.this);
+                    myDB.addTravel(t.stays.get(0).country, dataString);
+                    travel_id = new ArrayList<>();
                     travel_name = new ArrayList<>();
                     travel_info = new ArrayList<>();
 
+                    storeDataInArrays();
+
+
+                    System.out.println("To je travel info: "+ travel_info);
                     startActivity(new Intent(AddTravelActivity.this, MainActivity.class));
                 }
             }
         });
+    }
+
+    void storeDataInArrays()
+    {
+        Cursor cursor = myDB.readAllData();
+        if (cursor.getCount() == 0) {
+        } else {
+            while (cursor.moveToNext()) {
+                travel_id.add(cursor.getString(0));
+                travel_name.add(cursor.getString(1));
+                travel_info.add(cursor.getString(2));
+            }
+
+        }
     }
 }
